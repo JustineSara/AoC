1f09235feebf3a9c4113d5b6b57dcc1da6bd4ac7
Justine S. Pujos <pujos.justine.s@gmail.com>
2024-12-06 17:11:21 +0100

    input

--- src/t/d5.clj
+++ src/t/d5.clj
@@ -4,29 +4,57 @@
     [clojure.string :as cljstr]
     [clojure.set :as cljset]))
 
-  (def sample "")
+(def sample
+"47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
 
-  (defn parse-input
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47")
+
+(defn parse-input
   [input]
   (cljstr/split-lines input)
   )
 
 
-  (defn d5p1
+(defn d5p1
   [input]
   (let [x (parse-input input)
-  ]
-  x
-  ))
+        ]
+    x
+    ))
 
-  (defn d5p2
+(defn d5p2
   [input]
   (let [x (parse-input input)
-  ]
-  x
-  ))
+        ]
+    x
+    ))
 
-  (defn -main
+(defn -main
   [& args]
   (println "day5")
   (println sample)
@@ -41,4 +69,4 @@
   ;;  (prn (d5p2 sample))
   ;;  (prn (d5p2 (slurp "input/day5.txt")))
   )
-  
+
