80f05e03f0cd636e7213200614542a434aeab5e9
Justine S. Pujos <pujos.justine.s@gmail.com>
2024-12-07 17:45:22 +0100

    input

--- src/t/d7.clj
+++ src/t/d7.clj
@@ -4,29 +4,38 @@
     [clojure.string :as cljstr]
     [clojure.set :as cljset]))
 
-  (def sample "")
+(def sample
+"190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20")
 
-  (defn parse-input
+(defn parse-input
   [input]
   (cljstr/split-lines input)
   )
 
 
-  (defn d7p1
+(defn d7p1
   [input]
   (let [x (parse-input input)
-  ]
-  x
-  ))
+        ]
+    x
+    ))
 
-  (defn d7p2
+(defn d7p2
   [input]
   (let [x (parse-input input)
-  ]
-  x
-  ))
+        ]
+    x
+    ))
 
-  (defn -main
+(defn -main
   [& args]
   (println "day7")
   (println sample)
@@ -41,4 +50,4 @@
   ;;  (prn (d7p2 sample))
   ;;  (prn (d7p2 (slurp "input/day7.txt")))
   )
-  
+
