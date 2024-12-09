26837a7358d68b15c415e69afb10e3089c94e06b
Justine S. Pujos <pujos.justine.s@gmail.com>
2024-12-09 11:21:39 +0100

    generate template

---
+++ src/t/d9.clj
@@ -0,0 +1,44 @@
+(ns t.d9
+  (:gen-class)
+  (:require
+    [clojure.string :as cljstr]
+    [clojure.set :as cljset]))
+
+  (def sample "")
+
+  (defn parse-input
+  [input]
+  (cljstr/split-lines input)
+  )
+
+
+  (defn d9p1
+  [input]
+  (let [x (parse-input input)
+  ]
+  x
+  ))
+
+  (defn d9p2
+  [input]
+  (let [x (parse-input input)
+  ]
+  x
+  ))
+
+  (defn -main
+  [& args]
+  (println "day9")
+  (println sample)
+  (newline)
+
+  (println "part1")
+  (prn (d9p1 sample))
+  ;;  (prn (d9p1 (slurp "input/day9.txt")))
+
+  ;;  (newline)
+  ;;  (println "part2")
+  ;;  (prn (d9p2 sample))
+  ;;  (prn (d9p2 (slurp "input/day9.txt")))
+  )
+  
