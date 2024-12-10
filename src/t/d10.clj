0dc3d3103221b995fc4c0bc9be29183512d655a8
Justine S. Pujos <pujos.justine.s@gmail.com>
2024-12-10 12:17:06 +0100

    input

--- src/t/d10.clj
+++ src/t/d10.clj
@@ -5,29 +5,37 @@
     [clojure.set :as cljset]
     [clojure.core.match :refer [match]]))
 
-  (def sample "")
+(def sample
+"89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732")
 
-  (defn parse-input
+(defn parse-input
   [input]
   (cljstr/split-lines input)
   )
 
 
-  (defn d10p1
+(defn d10p1
   [input]
   (let [x (parse-input input)
-  ]
-  x
-  ))
+        ]
+    x
+    ))
 
-  (defn d10p2
+(defn d10p2
   [input]
   (let [x (parse-input input)
-  ]
-  x
-  ))
+        ]
+    x
+    ))
 
-  (defn -main
+(defn -main
   [& args]
   (println "day10")
   (println sample)
@@ -42,4 +50,4 @@
   ;;  (prn (d10p2 sample))
   ;;  (prn (d10p2 (slurp "input/day10.txt")))
   )
-  
+
