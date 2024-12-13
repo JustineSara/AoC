1379e397db63264c25ad227a5ae8903367a2e56f
Justine S. Pujos <pujos.justine.s@gmail.com>
2024-12-13 06:16:07 +0100

    input

--- src/t/d13.clj
+++ src/t/d13.clj
@@ -5,29 +5,45 @@
     [clojure.set :as cljset]
     [clojure.core.match :refer [match]]))
 
-  (def sample "")
+(def sample
+"Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
 
-  (defn parse-input
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279
+")
+
+(defn parse-input
   [input]
   (cljstr/split-lines input)
   )
 
 
-  (defn d13p1
+(defn d13p1
   [input]
   (let [x (parse-input input)
-  ]
-  x
-  ))
+        ]
+    x
+    ))
 
-  (defn d13p2
+(defn d13p2
   [input]
   (let [x (parse-input input)
-  ]
-  x
-  ))
+        ]
+    x
+    ))
 
-  (defn -main
+(defn -main
   [& args]
   (println "day13")
   (println sample)
@@ -42,4 +58,4 @@
   ;;  (prn (d13p2 sample))
   ;;  (prn (d13p2 (slurp "input/day13.txt")))
   )
-  
+
