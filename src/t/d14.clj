23040b1409658051cb0c4c33665a6e937c83a88b
Justine S. Pujos <pujos.justine.s@gmail.com>
2024-12-14 10:42:36 +0100

    parse input

--- src/t/d14.clj
+++ src/t/d14.clj
@@ -5,29 +5,43 @@
     [clojure.set :as cljset]
     [clojure.core.match :refer [match]]))
 
-  (def sample "")
+(def sample
+"p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3
+")
 
-  (defn parse-input
+(defn parse-input
   [input]
-  (cljstr/split-lines input)
-  )
+  (->> input
+       (re-seq #"p=(\d+),(\d+) v=(-?\d+),(-?\d+)")
+       (map (fn [[_ & r]] (map parse-long r) ))))
 
 
-  (defn d14p1
+(defn d14p1
   [input]
   (let [x (parse-input input)
-  ]
-  x
-  ))
+        ]
+    x
+    ))
 
-  (defn d14p2
+(defn d14p2
   [input]
   (let [x (parse-input input)
-  ]
-  x
-  ))
+        ]
+    x
+    ))
 
-  (defn -main
+(defn -main
   [& args]
   (println "day14")
   (println sample)
@@ -42,4 +56,4 @@
   ;;  (prn (d14p2 sample))
   ;;  (prn (d14p2 (slurp "input/day14.txt")))
   )
-  
+
