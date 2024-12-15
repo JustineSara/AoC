9557fe5f5799b847e6b5bf7456ce10e2a40cc8aa
Justine S. Pujos <pujos.justine.s@gmail.com>
2024-12-15 11:21:10 +0100

    input

---
+++ src/t/d15.clj
@@ -0,0 +1,81 @@
+(ns t.d15
+  (:gen-class)
+  (:require
+    [clojure.string :as cljstr]
+    [clojure.set :as cljset]
+    [clojure.core.match :refer [match]]))
+
+(def sample1
+"########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+<^^>>>vv<v>>v<<")
+
+(def sol1 2028)
+
+(def sample2
+"##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^")
+(def sol2 10092)
+
+(defn parse-input
+  [input]
+  (cljstr/split-lines input)
+  )
+
+
+(defn d15p1
+  [input]
+  (let [x (parse-input input)
+        ]
+    x
+    ))
+
+(defn d15p2
+  [input]
+  (let [x (parse-input input)
+        ]
+    x
+    ))
+
+(defn -main
+  [& args]
+  (println "day15")
+  (println sample)
+  (newline)
+
+  (println "part1")
+  (prn (d15p1 sample))
+  ;;  (prn (d15p1 (slurp "input/day15.txt")))
+
+  ;;  (newline)
+  ;;  (println "part2")
+  ;;  (prn (d15p2 sample))
+  ;;  (prn (d15p2 (slurp "input/day15.txt")))
+  )
+
