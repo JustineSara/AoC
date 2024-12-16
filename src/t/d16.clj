5e2e23539a2ce33c6846c6ab5f0651f05a7c405b
Justine S. Pujos <pujos.justine.s@gmail.com>
2024-12-16 11:59:50 +0100

    part 1

--- src/t/d16.clj
+++ src/t/d16.clj
@@ -48,16 +48,71 @@
 
 (defn parse-input
   [input]
-  (cljstr/split-lines input)
-  )
+  (->> input
+       cljstr/split-lines
+       (map-indexed
+         (fn [y l]
+           (apply merge-with into
+                  (map-indexed
+                    (fn [x c]
+                      (case c
+                        \S {:start [[x y]]}
+                        \# {:walls [[x y]]}
+                        \E {:end   [[x y]]}
+                        {}))
+                    l))))
+        (apply merge-with into)
+         ))
+
+
+(defn def-one-step
+  [walls move-score turn-score]
+  (fn [pos score d]
+    (let [n-pos (map + pos d)
+          n-dir (if (zero? (first d)) [[1 0] [-1 0]] [[0 1] [0 -1]])
+          res (map (fn [nd] [pos (+ score turn-score) nd]) n-dir)]
+      (if (contains? walls n-pos) res
+        (conj res [n-pos (+ score move-score) d])
+      )
+    )))
+
 
 
 (defn d16p1
   [input]
-  (let [x (parse-input input)
+  (let [{:keys [walls start end]} (parse-input input)
+        walls (set walls)
+        start (first start)
+        end (first end)
+        move-score 1
+        turn-score 1000
+        one-step (def-one-step walls move-score turn-score)
         ]
-    x
-    ))
+
+    #_(let [r [start 0 [1 0]]
+          r1 (apply one-step r)
+          r1 (sort-by second r1)
+          exp #{[start [1 0]]}
+          r2 (apply one-step (second r1))
+          ]
+      (prn r)
+      (prn r1)
+      (prn (filter (fn [[p _ d]] (not (contains? exp [p d]))) r1))
+      (prn r2)
+      (prn (filter (fn [[p _ d]] (not (contains? exp [p d]))) r2))
+      )
+
+    (loop [runners [[start 0 [1 0]]]
+           explored #{[start [1 0]]}]
+      (let [[r & runners] runners]
+        (if (= (first r) end) (second r)
+          (let [n-r (apply one-step r)
+                n-r (filter (fn [[p _ d]] (not (contains? explored [p d]))) n-r)
+                n-expl (cljset/union explored (set (map (fn [[p _ d]] [p d]) n-r)))]
+            (recur (sort-by second (concat runners n-r)) n-expl))
+       )
+      )
+    )))
 
 (defn d16p2
   [input]
@@ -69,12 +124,15 @@
 (defn -main
   [& args]
   (println "day16")
-  (println sample)
+  (println sample1)
   (newline)
 
   (println "part1")
-  (prn (d16p1 sample))
-  ;;  (prn (d16p1 (slurp "input/day16.txt")))
+  (prn (d16p1 sample1))
+  (prn [:sol1 sol1])
+  (prn (d16p1 sample2))
+  (prn [:sol2 sol2])
+  (prn (d16p1 (slurp "input/day16.txt")))
 
   ;;  (newline)
   ;;  (println "part2")
