6612b3291e3d23ade5c8cfca612c99e49ee42011
Justine S. Pujos <pujos.justine.s@gmail.com>
2024-12-18 10:17:44 +0100

    solved

--- src/t/d18.clj
+++ src/t/d18.clj
@@ -35,23 +35,56 @@
 
 (defn parse-input
   [input]
-  (cljstr/split-lines input)
-  )
+  (->> input
+       (re-seq #"(\d+),(\d+)")
+       (map (fn [[_ x y]] [(parse-long x) (parse-long y)]))
+       ))
 
+(defn corrupted-mem
+  [fall nfall]
+  (set (take nfall (cycle fall))))
+
+(def dir [[0 1] [0 -1] [1 0] [-1 0]])
+(defn find-way
+  [start end obst smax]
+  (loop [pos [start]
+         alreadythere #{start}
+         steps 0]
+    (if (empty? pos) -1
+;;    (prn [:steps steps :nexpl (count alreadythere) :npos (count pos)])
+    (let [npos (->> pos
+                    (mapcat (fn [p] (map (fn [d] (map + p d)) dir) ))
+                    (distinct)
+                    (filter (fn [p] (every? #(<= 0 % smax) p)))
+                    (filter (fn [p] (not (contains? obst p))))
+                    (filter (fn [p] (not (contains? alreadythere p))))
+                    )]
+      (if (some #(= % end) npos) (inc steps)
+        (recur npos (apply conj alreadythere npos) (inc steps)))
+      ))))
 
 (defn d18p1
-  [input smax]
-  (let [x (parse-input input)
+  [input smax nbytes]
+  (let [fall (parse-input input)
+        c-mem (corrupted-mem fall nbytes)
         ]
-    x
-    ))
+    (find-way [0 0] [smax smax] c-mem smax)))
+
+
 
 (defn d18p2
-  [input]
-  (let [x (parse-input input)
+  [input smax nbytes]
+  (let [fall (parse-input input)
+        c-mem (corrupted-mem fall nbytes)
+        r-fall (drop nbytes fall)
         ]
-    x
-    ))
+    (loop [falling r-fall
+           c-mem c-mem]
+      (let [[nf & falling] falling
+            c-mem (conj c-mem nf)]
+        (if (pos? (find-way [0 0] [smax smax] c-mem smax))
+          (recur falling c-mem)
+          nf)))))
 
 (defn -main
   [& args]
@@ -59,14 +92,17 @@
   (println sample)
   (newline)
 
+  (comment
   (println "part1")
   (prn (d18p1 sample 6 12))
   (prn sol)
-  ;;  (prn (d18p1 (slurp "input/day18.txt") 70 1024))
+  (prn (d18p1 (slurp "input/day18.txt") 70 1024))
+)
 
-  ;;  (newline)
-  ;;  (println "part2")
-  ;;  (prn (d18p2 sample))
-  ;;  (prn (d18p2 (slurp "input/day18.txt")))
+  (newline)
+  (println "part2")
+  (prn (d18p2 sample 6 12))
+  (prn "6,1")
+  (prn (d18p2 (slurp "input/day18.txt") 70 1024))
   )
 
