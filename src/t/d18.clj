3f8f1b28a43c2e6be6c5ef839c31d333b51a3b92
Justine S. Pujos <pujos.justine.s@gmail.com>
2024-12-18 08:33:23 +0100

    inputs

--- src/t/d18.clj
+++ src/t/d18.clj
@@ -5,41 +5,68 @@
     [clojure.set :as cljset]
     [clojure.core.match :refer [match]]))
 
-  (def sample "")
+(def sample
+"5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0")
+(def sol 22)
 
-  (defn parse-input
+(defn parse-input
   [input]
   (cljstr/split-lines input)
   )
 
 
-  (defn d18p1
+(defn d18p1
+  [input smax]
+  (let [x (parse-input input)
+        ]
+    x
+    ))
+
+(defn d18p2
   [input]
   (let [x (parse-input input)
-  ]
-  x
-  ))
+        ]
+    x
+    ))
 
-  (defn d18p2
-  [input]
-  (let [x (parse-input input)
-  ]
-  x
-  ))
-
-  (defn -main
+(defn -main
   [& args]
   (println "day18")
   (println sample)
   (newline)
 
   (println "part1")
-  (prn (d18p1 sample))
-  ;;  (prn (d18p1 (slurp "input/day18.txt")))
+  (prn (d18p1 sample 6 12))
+  (prn sol)
+  ;;  (prn (d18p1 (slurp "input/day18.txt") 70 1024))
 
   ;;  (newline)
   ;;  (println "part2")
   ;;  (prn (d18p2 sample))
   ;;  (prn (d18p2 (slurp "input/day18.txt")))
   )
-  
+
