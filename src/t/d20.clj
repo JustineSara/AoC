82d4c70aa49e7827ca10aaecdad34a744a9a1858
Justine S. Pujos <pujos.justine.s@gmail.com>
2024-12-20 11:22:59 +0100

    part1 too high, someone else answer :o

--- src/t/d20.clj
+++ src/t/d20.clj
@@ -48,12 +48,101 @@
   ))
 
 
+(defn inside?
+  [x y xmax ymax]
+  (and (< -1 x xmax) (< -1 y ymax)))
+
+(defn def-find-shortest-path
+  [walls E xmax ymax]
+  (fn [S]
+    (loop [p [S]
+           N 0]
+        (let [np (mapcat
+                   (fn [[x y]] [[(inc x) y] [(dec x) y] [x (inc y)] [x (dec y)]])
+                   p)
+              np (->> np
+                      (filter (fn [[x y]] (inside? x y xmax ymax)))
+                      (filter #(not (contains? walls %))))]
+          (if (some #(= % E) np) (inc N)
+            (recur np (inc N))
+            )))))
+
+(defn def-shortest-path-with-known
+  [walls E xmax ymax]
+  (fn [S known]
+    (loop [p [[0 S]]]
+        (let [[[n [x y]] & p] p]
+          (if (= [x y] E) n
+            (let [np (->> [[(inc x) y] [(dec x) y] [x (inc y)] [x (dec y)]]
+                          (filter (fn [[x y]] (inside? x y xmax ymax)))
+                          (filter #(not (contains? walls %)))
+                          (map (fn [pp] [(inc n) pp]))
+                          (map (fn [[np pp]] (if-let [npp (get known pp)] [(+ np npp) E] [np pp]))))]
+            (recur (sort-by first (concat p np)))
+            ))))))
+
+(defn around
+  [[x y]]
+  [[(inc x) y] [(dec x) y] [x (inc y)] [x (dec y)]])
+
+(defn all-sh-paths
+  [E walls xmax ymax]
+  (let [test-point (fn [[x y]] (and (inside? x y xmax ymax) (not (contains? walls [x y]))))]
+    (loop [pos (filter test-point (around E))
+           known {E 0}
+           dist 1]
+      (if (empty? pos) known
+        (let [npos (->> pos
+                        (mapcat around)
+                        (filter test-point)
+                        (filter (fn [p] (not (contains? known p)))))
+              nknown (->> pos
+                          (map (fn [p] {p dist}))
+                          (into {}))]
+          (recur npos (merge known nknown) (inc dist))
+          )))))
+(defn d20p1-sample
+  [input]
+  (let [{:keys [walls S E xmax ymax]} (parse-input input)
+        find-sh-path (def-shortest-path-with-known walls E xmax ymax)
+        known {}
+        toE (all-sh-paths E walls xmax ymax)
+        fromS (all-sh-paths S walls xmax ymax)
+        sh-path (get toE S)
+        test-point (fn [[x y]] (and (inside? x y xmax ymax) (not (contains? walls [x y]))))
+        ]
+    (prn sh-path)
+    (frequencies
+      (for [[wx wy] walls
+            j [[[(dec wx) wy] [(inc wx) wy]]
+               [[(inc wx) wy] [(dec wx) wy]]
+               [[wx (dec wy)] [wx (inc wy)]]
+               [[wx (inc wy)] [wx (dec wy)]]]
+            :when (every? test-point j)
+            :when (< (+ (get fromS (first j)) (get toE (second j)) 2) sh-path)]
+        (- sh-path (+ (get fromS (first j)) (get toE (second j)) 2) )))))
+
 (defn d20p1
   [input]
-  (let [x (parse-input input)
+  (let [{:keys [walls S E xmax ymax]} (parse-input input)
+        find-sh-path (def-shortest-path-with-known walls E xmax ymax)
+        known {}
+        toE (all-sh-paths E walls xmax ymax)
+        fromS (all-sh-paths S walls xmax ymax)
+        sh-path (get toE S)
+        test-point (fn [[x y]] (and (inside? x y xmax ymax) (not (contains? walls [x y]))))
         ]
-    x
-    ))
+    (prn sh-path)
+    (apply +
+           (for [[wx wy] walls
+                 j [[[(dec wx) wy] [(inc wx) wy]]
+                    [[(inc wx) wy] [(dec wx) wy]]
+                    [[wx (dec wy)] [wx (inc wy)]]
+                    [[wx (inc wy)] [wx (dec wy)]]]
+                 :when (every? test-point j)
+                 :when (<= 100 (- (+ (get fromS (first j)) (get toE (second j)) 2) sh-path))]
+             1
+             ))))
 
 (defn d20p2
   [input]
@@ -69,8 +158,11 @@
   (newline)
 
   (println "part1")
-  (prn (d20p1 sample))
-  ;;  (prn (d20p1 (slurp "input/day20.txt")))
+  (prn (d20p1-sample sample))
+  (prn (d20p1 (slurp "input/day20.txt")))
+  ;; 1438
+  ;; That's not the right answer; your answer is too high.
+  ;; Curiously, it's the right answer for someone else; you might be logged in to the wrong account or just unlucky.
 
   ;;  (newline)
   ;;  (println "part2")
