f731a1a5f51886c6f40b01bbc50d71f3cfa7e03c
Justine S. Pujos <pujos.justine.s@gmail.com>
2024-12-22 11:01:35 +0100

    part 1

--- src/t/d22.clj
+++ src/t/d22.clj
@@ -14,16 +14,78 @@
 
 (defn parse-input
   [input]
-  (cljstr/split-lines input)
+  (->> input
+  (cljstr/split-lines)
+  (map parse-long)
+  ))
+
+
+(def mix bit-xor)
+(comment
+  (mix 42 15)
+  ;; 37
   )
 
+(defn prune
+  [v]
+  (mod v 16777216))
+(comment
+  (prune 100000000)
+  ;; 16113920
+  )
+
+(defn op1
+  [s]
+  (-> s
+      (* 64)
+      (mix s)
+      prune))
+(comment
+  (op1 123)
+  ;; nope : 15887950
+  ;; 7867
+  )
+
+(defn op2
+  [s]
+  (-> s
+      (quot 32)
+      (mix s)
+      prune))
+
+(defn op3
+  [s]
+  (-> s
+      (* 2048)
+      (mix s)
+      prune))
+
+(defn next-sn
+  [s]
+  (-> s
+      op1
+      op2
+      op3))
+(comment
+  (next-sn (next-sn 123))
+  (prune 123)
+  )
+
+
+(comment
+  (def insn (iterate next-sn 1))
+  (nth insn 2000)
+
+  )
 
 (defn d22p1
   [input]
   (let [x (parse-input input)
         ]
-    x
-    ))
+    (apply +
+           (for [sn x]
+             (nth (iterate next-sn sn) 2000))
+           )))
 
 (defn d22p2
   [input]
@@ -40,7 +102,8 @@
 
   (println "part1")
   (prn (d22p1 sample))
-  ;;  (prn (d22p1 (slurp "input/day22.txt")))
+  (prn sol)
+  (prn (d22p1 (slurp "input/day22.txt")))
 
   ;;  (newline)
   ;;  (println "part2")
