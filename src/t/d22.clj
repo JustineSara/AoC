36351c92751354d8f752885f34b6bbe2c83c7590
Justine S. Pujos <pujos.justine.s@gmail.com>
2024-12-22 10:25:03 +0100

    generate and input

---
+++ src/t/d22.clj
@@ -0,0 +1,50 @@
+(ns t.d22
+  (:gen-class)
+  (:require
+    [clojure.string :as cljstr]
+    [clojure.set :as cljset]
+    [clojure.core.match :refer [match]]))
+
+(def sample
+"1
+10
+100
+2024")
+(def sol 37327623)
+
+(defn parse-input
+  [input]
+  (cljstr/split-lines input)
+  )
+
+
+(defn d22p1
+  [input]
+  (let [x (parse-input input)
+        ]
+    x
+    ))
+
+(defn d22p2
+  [input]
+  (let [x (parse-input input)
+        ]
+    x
+    ))
+
+(defn -main
+  [& args]
+  (println "day22")
+  (println sample)
+  (newline)
+
+  (println "part1")
+  (prn (d22p1 sample))
+  ;;  (prn (d22p1 (slurp "input/day22.txt")))
+
+  ;;  (newline)
+  ;;  (println "part2")
+  ;;  (prn (d22p2 sample))
+  ;;  (prn (d22p2 (slurp "input/day22.txt")))
+  )
+
