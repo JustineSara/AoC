cac64a0950dab6394b5f8327e42cf2d3d4a40cf9
Justine S. Pujos <pujos.justine.s@gmail.com>
2024-12-23 12:37:32 +0100

    input

--- src/t/d23.clj
+++ src/t/d23.clj
@@ -5,29 +5,61 @@
     [clojure.set :as cljset]
     [clojure.core.match :refer [match]]))
 
-  (def sample "")
+(def sample
+"kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn")
 
-  (defn parse-input
+(defn parse-input
   [input]
   (cljstr/split-lines input)
   )
 
 
-  (defn d23p1
+(defn d23p1
   [input]
   (let [x (parse-input input)
-  ]
-  x
-  ))
+        ]
+    x
+    ))
 
-  (defn d23p2
+(defn d23p2
   [input]
   (let [x (parse-input input)
-  ]
-  x
-  ))
+        ]
+    x
+    ))
 
-  (defn -main
+(defn -main
   [& args]
   (println "day23")
   (println sample)
@@ -42,4 +74,4 @@
   ;;  (prn (d23p2 sample))
   ;;  (prn (d23p2 (slurp "input/day23.txt")))
   )
-  
+
